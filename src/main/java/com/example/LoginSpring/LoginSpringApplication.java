6c263032072fc9b9a1525af2b3217fb32c52a758
DiegoX <diegomenendez17@gmail.com>
2023-06-14 10:15:10 -0600

    Comenzando la estructura de la Api de Login

---
+++ src/main/java/com/example/LoginSpring/LoginSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoginSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoginSpring/LoginSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoginSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
